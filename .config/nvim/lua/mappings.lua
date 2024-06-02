3d106f61b7e1f3fe58cd2d48b188974cc5ea3ae7
Ibrahim Abdel Rahman <IbrahimARahman05@gmail.com>
2024-06-02 10:28:32 -0500

    most tmux stuff, still no lsp

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,3 +5,9 @@ vim.api.nvim_set_keymap('n', 'J', ':m .+1<CR>==', { noremap = true, silent = tru
 -- Move 1 more line up or down in visual mode
 vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
