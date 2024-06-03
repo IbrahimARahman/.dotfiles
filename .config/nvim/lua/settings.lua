db9324393e0649d2928d56c7408b18732e831dbd
Ibrahim Abdel Rahman <IbrahimARahman05@gmail.com>
2024-06-02 19:12:23 -0500

    idk, some settings

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -8,3 +8,27 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+-- Create the augroup 'setIndent' and clear it if it already exists
+local setIndentGroup = vim.api.nvim_create_augroup('setIndent', { clear = true })
+
+-- Create the autocmd for setting indent options
+vim.api.nvim_create_autocmd('FileType', {
+  group = setIndentGroup,
+  pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript', 'yaml', 'lua', 'jsx', 'tsx', 'typescriptreact', 'javascriptreact' },
+  command = 'setlocal shiftwidth=2 tabstop=2'
+})
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
