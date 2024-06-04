bae811d47412020492f074a4f13653a0d5c3170d
Ibrahim Abdel Rahman <IbrahimARahman05@gmail.com>
2024-06-04 02:34:32 -0500

    everything besides lsp, important plugins done

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,27 @@ return {
   tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require("telescope").setup{
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+          '--glob', '!.git/*',
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          file_ignore_patterns = {".git/"}
+        },
+      },
+    }
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -16,12 +16,17 @@ local setIndentGroup = vim.api.nvim_create_augroup('setIndent', { clear = true }
 vim.api.nvim_create_autocmd('FileType', {
   group = setIndentGroup,
   pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript', 'yaml', 'lua', 'jsx', 'tsx', 'typescriptreact', 'javascriptreact' },
-  command = 'setlocal shiftwidth=2 tabstop=2'
+  command = 'setlocal shiftwidth=2 tabstop=2 softtabstop=2'
 })
 
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.env.HOME  .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
